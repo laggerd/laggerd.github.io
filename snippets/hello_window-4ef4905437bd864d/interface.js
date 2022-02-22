c4d44a095c32f344c278af7214de96d7e6f7aab7
laggerd <71416470+laggerd@users.noreply.github.com>
2022-02-22 16:38:22 -0600

    mandel final pog

---
+++ snippets/hello_window-4ef4905437bd864d/interface.js
@@ -0,0 +1,21 @@
+export function check_function() {
+    console.log("f");
+    return window.my_string;
+}
+
+export function check_coloring() {
+    console.log("k");
+    return window.coloring_exponent;
+}
+
+export function check_iterations() {
+    console.log("c");
+    return window.iterations;
+}
+
+export function reset_function() {
+    window.my_string = null;
+    window.coloring_exponent = null;
+    window.iterations = null;
+}
+//r= a(1 - e cos(E))
